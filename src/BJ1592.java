ec4b924b34acad2e16378e93a99ba319a3ff9245
vrc0103 <tajja000@naver.com>
2024-07-27 10:35:54 +0900

    영식이와 친구들

---
+++ src/BJ1592.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ1592 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int num, max, len;
+    static int res;
+    static int[] count;
+
+    public static void main(String[] args) throws IOException {
+        getArr();
+
+        System.out.println(res);
+    }
+
+    static void getArr() throws IOException {
+        int loc;
+
+        st = new StringTokenizer(br.readLine());
+        num = Integer.parseInt(st.nextToken());
+        max = Integer.parseInt(st.nextToken());
+        len = Integer.parseInt(st.nextToken());
+
+        res = 0;
+        loc = 0;
+        count = new int[num];
+        count[loc]++;
+
+        while (count[loc] != max) {
+            if (count[loc] % 2 == 1) {
+                loc = (loc + len) % num;
+
+            } else {
+                loc = loc - len < 0 ? loc - len + num : loc - len;
+            }
+
+            count[loc]++;
+            res++;
+        }
+    }
+}
