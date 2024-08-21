d283d7c4ae56a1f7a2ca516ee60ef7761538fd87
vrc0103 <tajja000@naver.com>
2024-08-21 16:03:10 +0900

    백준

---
+++ src/BOJ11279.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ11279 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int len = Integer.parseInt(br.readLine().trim());
+        long input;
+        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < len; i++) {
+            input = Long.parseLong(br.readLine().trim());
+
+            if (input == 0) {
+                if (!pq.isEmpty()) {
+                    sb.append(pq.poll()).append("\n");
+                } else {
+                    sb.append("0\n");
+                }
+            } else {
+                pq.add(input);
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
---
+++ src/BOJ1874.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ1874 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        System.out.print(judgeStack());
+    }
+
+    static String judgeStack() throws IOException {
+        int len = Integer.parseInt(br.readLine().trim());
+        int input;
+        int num = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < len; i++) {
+            input = Integer.parseInt(br.readLine().trim());
+
+            if (num < input) {
+                while (num < input) {
+                    num++;
+                    stack.push(num);
+                    sb.append("+\n");
+                }
+
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                if (stack.isEmpty() || stack.peek() < input) {
+                    return "NO\n";
+                }
+                while (!stack.isEmpty() && stack.peek() >= input) {
+                    stack.pop();
+                    sb.append("-\n");
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
