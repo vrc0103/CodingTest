a844c71ef2663a434cc9990dd2b09f50e61dd68d
vrc0103 <tajja000@naver.com>
2024-09-05 15:51:51 +0900

    1로 만들기 / DP
    재귀로 구현하더라도 num - 1에 대한 비교가 항상필요하므로 배열의 모든 값을 탐색하게 됨
    -> 반복문을 써서 2부터 순서대로 올라오는게 더 빠름

---
+++ src/BOJ1463.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ1463 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int res;
+    static int input;
+    static int[] cnt;
+
+    public static void main(String[] args) throws IOException {
+        getMin();
+
+        System.out.println(res);
+    }
+
+    static void getMin() throws IOException {
+        input = Integer.parseInt(br.readLine().trim());
+
+        if (input == 1) {
+            res = 0;
+            return;
+        }
+
+        cnt = new int[input + 1];
+
+        dp_loop();
+    }
+
+    static void dp_loop() {
+        cnt[1] = 0;
+
+        for (int i = 2; i <= input; i++) {
+            cnt[i] = cnt[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                cnt[i] = Math.min(cnt[i], cnt[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                cnt[i] = Math.min(cnt[i], cnt[i / 3] + 1);
+            }
+        }
+
+        res = cnt[input];
+    }
+
+    static int dp(int num) {
+        // 이미 계산된 숫자 -> 바로 반환
+        // 1은 횟수가 0 -> 바로 반환
+        if (cnt[num] > 0 || num == 1) {
+            return cnt[num];
+        }
+
+        // 계산이 완료되지 않은 숫자
+        if (num % 3 == 0 && num % 2 == 0) {
+            cnt[num] = Math.min(dp(num - 1), Math.min(dp(num / 3), dp(num / 2))) + 1;
+        } else if (num % 3 == 0) {
+            cnt[num] = Math.min(dp(num / 3), dp(num - 1)) + 1;
+        } else if (num % 2 == 0) {
+            cnt[num] = Math.min(dp(num / 2), dp(num - 1)) + 1;
+        } else {
+            cnt[num] = dp(num - 1) + 1;
+        }
+
+        return cnt[num];
+    }
+}
