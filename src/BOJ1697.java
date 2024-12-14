ed624f86438b880ece347bf2eebef75e08ae1cb3
vrc0103 <tajja000@naver.com>
2024-12-14 12:18:26 +0900

    숨바꼭질 / BFS
    1차원 탐색에도 최단경로 탐색은 BFS 이용

---
+++ src/BOJ1697.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ1697 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int res;
+    static int start, goal;
+
+    public static void main(String[] args) throws IOException {
+        getRes();
+
+        System.out.println(res);
+    }
+
+    static void getRes() throws IOException {
+        int now;
+        int[] len = new int[100001];
+        Queue<Integer> queue = new LinkedList<>();
+
+        st = new StringTokenizer(br.readLine().trim());
+        start = Integer.parseInt(st.nextToken());
+        goal = Integer.parseInt(st.nextToken());
+
+        Arrays.fill(len, -1);
+        queue.add(start);
+        len[start] = 0;
+
+        while (true) {
+            now = queue.remove();
+
+            if (now == goal) {
+                res = len[now];
+                return;
+            }
+
+            if (now + 1 <= 100000 && len[now + 1] == -1) {
+                len[now + 1] = len[now] + 1;
+                queue.add(now + 1);
+            }
+            if (now - 1 >= 0 && len[now - 1] == -1) {
+                len[now - 1] = len[now] + 1;
+                queue.add(now - 1);
+            }
+            if (now * 2 <= 100000 && len[now * 2] == -1) {
+                len[now * 2] = len[now] + 1;
+                queue.add(now * 2);
+            }
+        }
+    }
+}
