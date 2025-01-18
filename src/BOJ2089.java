adaf4e04297b67e2b8dfab35a2b32da8773cacd2
vrc0103 <tajja000@naver.com>
2025-01-18 11:30:12 +0900

    -2진수

---
+++ src/BOJ2089.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class BOJ2089 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static int num;
+
+    public static void main(String[] args) throws Exception {
+        getRes();
+
+        System.out.println(sb.reverse());
+    }
+
+    static void getRes() throws Exception {
+
+        num = Integer.parseInt(br.readLine().trim());
+
+        if (num == 0) {
+            sb.append("0");
+        } else {
+            while (num != 1) {
+                sb.append(Math.abs(num % -2));
+                num = (int) (Math.ceil((double) num / -2));
+            }
+
+            sb.append(num);
+        }
+    }
+}
