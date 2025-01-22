a1d346065719807b570785225b400a3d1dea4435
vrc0103 <tajja000@naver.com>
2025-01-22 22:42:59 +0900

    A와 B 2 / 문자열

--- src/BOJ12919.java
+++ src/BOJ12919.java
@@ -6,12 +6,13 @@ public class BOJ12919 {
     static StringTokenizer st;
 
     static int res = 0;
-    static String origin, change;
+    static StringBuilder origin = new StringBuilder();
+    static StringBuilder change = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         // 정보
-        origin = br.readLine().trim();
-        change = br.readLine().trim();
+        origin.append(br.readLine().trim());
+        change.append(br.readLine().trim());
 
         // 풀이
         DFS(change);
@@ -19,8 +20,10 @@ public class BOJ12919 {
         System.out.println(res);
     }
 
-    static void DFS(String sb) {
-        if (sb.toString().equals(origin.toString())) {
+    static void DFS(StringBuilder sb) {
+        // toString()도 String 객체를 생성하는 메서드라서 시간이 꽤 걸림
+        // -> 최소한으로 호출하는 게 좋음
+        if (sb.length() == origin.length() && sb.toString().equals(origin.toString())) {
             res = 1;
 
             return;
@@ -34,21 +37,17 @@ public class BOJ12919 {
 
         // 마지막 문자가 A이면 삭제
         if (sb.charAt(len - 1) == 'A' && res == 0) {
-            StringBuilder tmp = new StringBuilder();
-            tmp.append(sb);
-
-            DFS(tmp.deleteCharAt(len - 1).toString());
-            tmp.append('A');
+            // deleteCharAt()는 in-place 메서드이므로 다시 A를 추가해야 함
+            DFS(sb.deleteCharAt(len - 1));
+            sb.append('A');
         }
 
         // 첫 문자가 B이면 B 삭제 후 reverse
         if (sb.charAt(0) == 'B' && res == 0) {
-            StringBuilder tmp = new StringBuilder();
-            tmp.append(sb);
-
-            tmp.deleteCharAt(0);
-            DFS(tmp.reverse().toString());
-            tmp.append('B');
+            // reverse()도 마찬가지로 in-place 메서드라서 다시 돌려놔야 함
+            sb.deleteCharAt(0).reverse();
+            DFS(sb);
+            sb.append('B').reverse();
         }
     }
 }
