2fa8f66cb471302de15e6a64e9123fac61fb6cd4
Yechan Shin <tajja000@naver.com>
2025-02-11 21:05:24 +0900

    [Silver IV] Title: ATM, Time: 68 ms, Memory: 11832 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int num;
+    static int res;
+    static int[] time;
+
+    public static void main(String[] args) throws IOException {
+        getMinTime();
+
+        System.out.println(res);
+    }
+
+    static void getMinTime() throws IOException {
+        res = 0;
+        num = Integer.parseInt(br.readLine());
+        time = new int[num];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < num; i++) {
+            time[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time);
+
+        for (int i = 0; i < num; i++) {
+            res += time[i] * (num - i);
+        }
+    }
+}
