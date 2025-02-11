412d310175473701cffb1123ce37fe15d76b21b4
Yechan Shin <tajja000@naver.com>
2025-02-11 21:01:54 +0900

    [Silver I] Title: 정수 삼각형, Time: 180 ms, Memory: 24056 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int height;
+    static int res;
+    static int[][] triangle;
+
+    public static void main(String[] args) throws IOException {
+        getMax();
+
+        System.out.println(res);
+    }
+
+    static void getMax() throws IOException {
+        height = Integer.parseInt(br.readLine().trim()) + 1;
+
+        triangle = new int[height][height];
+
+        for (int h = 1; h < height; h++) {
+            st = new StringTokenizer(br.readLine().trim());
+
+            // 윗 행의 동일한 열, 앞 열 중 큰 값을 더해가며 누적합 저장
+            for (int l = 1; l < h + 1; l++) {
+                triangle[h][l] = Integer.parseInt(st.nextToken())
+                        + Math.max(triangle[h - 1][l], triangle[h - 1][l - 1]);
+            }
+        }
+
+        res = 0;
+        for (int l = 1; l < height; l++) {
+            res = Math.max(res, triangle[height - 1][l]);
+        }
+    }
+}
