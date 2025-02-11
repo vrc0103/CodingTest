110b5085dfd7225ee1d98789d4358e91cb6a72db
Yechan Shin <tajja000@naver.com>
2025-02-11 21:48:26 +0900

    [Bronze III] Title: ZOAC 4, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int res;
+    static int row, col;
+    static int rowGap, colGap;
+
+    public static void main(String[] args) throws IOException {
+        getRes();
+
+        System.out.println(res);
+    }
+
+    static void getRes() throws IOException {
+        int rowPos, colPos;
+
+        st = new StringTokenizer(br.readLine().trim());
+
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+        rowGap = Integer.parseInt(st.nextToken()) + 1;
+        colGap = Integer.parseInt(st.nextToken()) + 1;
+
+        // gap으로 나눈 나머지가 0 이상이면 한명 더 가능
+        rowPos = row / rowGap;
+        rowPos += row % rowGap > 0 ? 1 : 0;
+
+        colPos = col / colGap;
+        colPos += col % colGap > 0 ? 1 : 0;
+
+        res = rowPos * colPos;
+    }
+}
