f7b9398a8e2b38fbb92b2f2e878898ef6aab1066
Yechan Shin <tajja000@naver.com>
2025-02-11 22:40:46 +0900

    등급별 문제 개수 추가

--- update.py
+++ update.py
@@ -41,7 +41,7 @@ def main():
             if directory in ["백준", "프로그래머스"]:
                 content += "## 📚 {}\n".format(directory)
             else:
-                content += "### 🚀 {}\n".format(directory)
+                content += "### 🚀 {} ({}문제)\n".format(directory, len(subfolders))
                 content += "| 문제번호 | 링크 |\n"
                 content += "| ----- | ----- |\n"
             directories.append(directory)
