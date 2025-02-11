7929aff6672876dad1fc2225652092528215989c
Yechan Shin <tajja000@naver.com>
2025-02-11 21:08:54 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb.append(br.readLine()).append("??!");
+
+        System.out.println(sb);
+    }
+}
