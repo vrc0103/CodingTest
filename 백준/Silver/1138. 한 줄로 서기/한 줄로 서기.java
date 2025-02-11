2614a9705da4839e567ecfed4fa81507b21b26e1
Yechan Shin <tajja000@naver.com>
2025-02-11 21:47:54 +0900

    [Silver II] Title: 한 줄로 서기, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int num;
+    static int[] count;
+
+    public static void main(String[] args) throws IOException {
+        setInfo();
+
+        getRes();
+
+        System.out.print(sb.toString());
+    }
+
+    static void setInfo() throws IOException {
+        num = Integer.parseInt(br.readLine().trim());
+
+        count = new int[num + 1];
+        st = new StringTokenizer(br.readLine().trim());
+
+        for (int i = 1; i <= num; i++) {
+            count[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void getRes() {
+        LinkedList<Integer> order = new LinkedList<>();
+        int loc;
+
+        for (int i = num; i > 0; i--) {
+            loc = count[i];
+
+            order.add(loc, i);
+        }
+
+        for (int i = 0; i < num; i++) {
+            sb.append(order.get(i)).append(" ");
+        }
+        sb.append("\n");
+    }
+}
