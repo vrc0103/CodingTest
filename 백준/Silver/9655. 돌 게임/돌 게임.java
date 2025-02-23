79fa94b4f3ea6edb88022346cbd16f1c232b750b
Yechan Shin <tajja000@naver.com>
2025-02-23 14:28:00 +0900

    [Silver V] Title: 돌 게임, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static String res;
+    static int num;
+
+    public static void main(String[] args) throws IOException {
+        getRes();
+
+        System.out.println(res);
+    }
+
+    public static void getRes() throws IOException {
+        int turn;
+        int remain;
+
+        num = Integer.parseInt(br.readLine().trim());
+
+        // 1개 또는 3개 가져감 -> (3n+1)번째 돌은 항상 번갈아가며 선택하게 됨 -> 3으로 나눠도 무방함
+        // 나머지 돌은 1개씩만 가져갈 수 있음
+        turn = num / 3;
+        remain = num % 3;
+
+        turn += remain;
+
+        if (turn % 2 == 1) {
+            res = "SK";
+        } else {
+            res = "CY";
+        }
+    }
+}
