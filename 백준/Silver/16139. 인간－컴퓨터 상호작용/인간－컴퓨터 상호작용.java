43c4928dc3bca14cfe9ef3652f16ec6a021929bf
Yechan Shin <tajja000@naver.com>
2025-02-25 17:09:36 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 612 ms, Memory: 113968 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -8,6 +8,7 @@ public class Main {
 
     static String input;
     static int oper;
+    static int[][] cnt;
 
     public static void main(String[] args) throws Exception {
         // System.setIn(new FileInputStream("testCase.txt"));
@@ -15,24 +16,29 @@ public class Main {
         sb = new StringBuilder();
 
         input = br.readLine().trim();
+        cnt = new int[input.length() + 1][26];
+
+        for(int i = 1; i <= input.length(); i++) {
+            for(int j = 0; j < 26; j++) {
+                cnt[i][j] = cnt[i - 1][j];
+            }
+            
+            cnt[i][input.charAt(i - 1) - 'a']++;
+        }
 
         oper = Integer.parseInt(br.readLine().trim());
 
         while(oper-- > 0) {
-            int cnt = 0;
+            int count = 0;
 
             st = new StringTokenizer(br.readLine().trim());
-            char ch = st.nextToken().charAt(0);
+            int idx = st.nextToken().charAt(0) - 'a';
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            for(int i = start; i <= end; i++) {
-                if(input.charAt(i) == ch) {
-                    cnt++;
-                }
-            }
+            count = cnt[end + 1][idx] - cnt[start][idx];
 
-            sb.append(cnt).append("\n");
+            sb.append(count).append("\n");
         }
 
         System.out.print(sb);
