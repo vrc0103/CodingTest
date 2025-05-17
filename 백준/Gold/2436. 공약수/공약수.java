da11ccadb7ce9bf26de561880bb8dc6f368895a7
Yechan Shin <tajja000@naver.com>
2025-05-17 12:41:08 +0900

    [Gold V] Title: 공약수, Time: 68 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Gold/2436. 공약수/공약수.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    static int max, min;
+    static int mid;
+
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        getRes();
+
+        System.out.println(sb);
+    }
+
+    static void getRes() throws Exception {
+        st = new StringTokenizer(br.readLine().trim());
+        max = Integer.parseInt(st.nextToken());
+        min = Integer.parseInt(st.nextToken());
+
+        int num = min / max;
+        
+        int mid = (int) Math.sqrt(num);
+        for(int i = mid; i > 0; i--) {
+            if(num % i == 0) {
+                int a = num / i;
+                int b = i;
+
+                while(b > 1 && a % b != 0) {
+                    int tmp = a;
+
+                    if(a - b > b) {
+                        a = a - b;
+                    } else {
+                        a = b;
+                        b = tmp - b;
+                    }
+                }
+
+                if(b <= 1) {
+                    sb.append(i * max).append(" ").append(num / i * max);
+                    break;
+                }
+            }
+        }
+    }
+}
+
