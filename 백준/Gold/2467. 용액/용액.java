334c3432b737478dd058e4a1c890345add41fb11
Yechan Shin <tajja000@naver.com>
2025-06-14 16:14:57 +0900

    [Gold V] Title: 용액, Time: 260 ms, Memory: 32244 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -7,7 +7,7 @@ public class Main {
     static StringBuilder sb;
 
     static int num;
-    static List<Integer> value;
+    static int[] value;
 
     public static void main(String[] args) throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,11 +22,11 @@ public class Main {
 
     static void setInfo() throws Exception {
         num = Integer.parseInt(br.readLine().trim());
-        value = new ArrayList<>();
+        value = new int[num];
 
         st = new StringTokenizer(br.readLine().trim());
         for (int i = 0; i < num; i++) {
-            value.add(Integer.parseInt(st.nextToken()));
+            value[i] = Integer.parseInt(st.nextToken());
         }
     }
 
@@ -36,33 +36,33 @@ public class Main {
         int min = Integer.MAX_VALUE;
 
         for (int i = 0; i < num; i++) {
-            int now = value.get(i);
-            int idx = Collections.binarySearch(value, now * (-1));
+            int now = value[i];
+            int idx = Arrays.binarySearch(value, now * (-1));
 
             if (idx >= 0) {
-                n1 = value.get(i) < value.get(idx) ? value.get(i) : value.get(idx);
-                n2 = value.get(i) < value.get(idx) ? value.get(idx) : value.get(i);
+                n1 = value[i] < value[idx] ? value[i] : value[idx];
+                n2 = value[i] < value[idx] ? value[idx] : value[i];
 
                 break;
             } else {
                 int near = idx * (-1) - 1;
 
-                int sum = near < num ? Math.abs(value.get(i) + value.get(near)) : Integer.MAX_VALUE;
+                int sum = near < num ? Math.abs(value[i] + value[near]) : Integer.MAX_VALUE;
 
                 if (min > sum && near != i) {
                     min = sum;
 
-                    n1 = value.get(i) < value.get(near) ? value.get(i) : value.get(near);
-                    n2 = value.get(i) < value.get(near) ? value.get(near) : value.get(i);
+                    n1 = value[i] < value[near] ? value[i] : value[near];
+                    n2 = value[i] < value[near] ? value[near] : value[i];
                 }
 
-                sum = near > 0 ? Math.abs(value.get(i) + value.get(near - 1)) : Integer.MAX_VALUE;
+                sum = near > 0 ? Math.abs(value[i] + value[near - 1]) : Integer.MAX_VALUE;
 
                 if (min > sum && near - 1 != i) {
                     min = sum;
 
-                    n1 = value.get(i) < value.get(near - 1) ? value.get(i) : value.get(near - 1);
-                    n2 = value.get(i) < value.get(near - 1) ? value.get(near - 1) : value.get(i);
+                    n1 = value[i] < value[near - 1] ? value[i] : value[near - 1];
+                    n2 = value[i] < value[near - 1] ? value[near - 1] : value[i];
                 }
             }
         }
